c1272296e84f8e5d86652640bf2339e7ee3aa7c9
FerMortaro <mortarofernanda@gmail.com>
2023-01-27 14:59:40 -0300

    add project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,6 +2,7 @@ import { Container, Row, Col, Tab } from "react-bootstrap";
 import { Card } from './Card'
 import projImg1 from "../assets/img/dogsApp.png";
 import projImg2 from "../assets/img/kaloApp.png";
+import projImg3 from "../assets/img/refrigeracion.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import "animate.css";
 import TrackVisibility from "react-on-screen";
@@ -9,11 +10,11 @@ import TrackVisibility from "react-on-screen";
 export const Projects = () => {
   const projects = [
     {
-      title: "Dogs App",
+      title: "Refrigeración SJG",
       description:
-        "Desarrollo de una SPA, en la cual se consume información de una API de razas de perros. Se pueden filtrar las razas de perros por nombre, temperamento, y creación. También se pueden ordenar alfabéticamente y por peso, buscar una raza específica y obtener su información detallada o crear su propio perro en una base de datos.",
-      imgUrl: projImg1,
-      link:"https://pi-dogs-rust-seven.vercel.app/"
+      "Desarrollo de sitio corporativo para una empresa dedicada al mantenimiento/reparación de sistemas de refrigeración. Realizado con Next.js, TypeScript y Chakra UI",
+      imgUrl: projImg3,
+      link: "https://refrigeracionsjg.vercel.app/",
     },
     {
       title: "Kalo",
@@ -21,6 +22,13 @@ export const Projects = () => {
       "¡Bienvenido a Kalo! ¡Un Market Place diseñado para ti!. Logrado en 5 semanas de trabajo en grupo como prueba final para el bootcamp de Henry. Desarrollado con las últimas tecnologías y modulado para mejorar la efectividad en el rendimiento y garantizar que el código sea reutilizable y desarrollado con una gran experiencia UX/UI.",
       imgUrl: projImg2,
       link: "https://kalo-arg.vercel.app/",
+    },
+    {
+      title: "Dogs App",
+      description:
+        "Desarrollo de una SPA, en la cual se consume información de una API de razas de perros. Se pueden filtrar las razas de perros por nombre, temperamento, y creación. También se pueden ordenar alfabéticamente y por peso, buscar una raza específica y obtener su información detallada o crear su propio perro en una base de datos.",
+      imgUrl: projImg1,
+      link:"https://pi-dogs-fer0705.vercel.app/"
     },
   ];
 
